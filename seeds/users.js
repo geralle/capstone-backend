943148b7e44c88ded71d540adaf033a47a2a6047
geralle <gerallestes@gmail.com>
2018-01-28 19:53:04 -0700

    crisis on the seeds

--- seeds/users.js
+++ seeds/users.js
@@ -11,6 +11,13 @@ exports.seed = function(knex, Promise) {
           email: 'anothergiftcard@gmail.com',
           password: 'password123',
           phone_number: '1234567890'
+        },
+        {
+          f_name: 'geralle',
+          l_name: 'estes',
+          email: 'sdzgrail@gmail.com',
+          password: 'pass',
+          phone_number: '1234567890'
         }
       ]);
     });
